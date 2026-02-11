aceb99b682f1fbaaba83c34a80aac15ca43b92c0
xylxi <377793267@qq.com>
2026-02-11 21:50:16 +0800

    init

---
+++ QRDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRDemo
+//
+//  Created by ruirui on 2026/2/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
